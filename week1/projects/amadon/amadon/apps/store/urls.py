577751df895a8993322cb08302c0d693c6df9591
mikegerrity <mikegerrity@hotmail.com>
2017-10-13 15:30:43 -0400

    Updated file structure, hopefully

--- week1/projects/amadon/amadon/apps/store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^amadon$', views.amadon),
-    url(r'^checkout$', views.checkout),
-    url(r'^clear$', views.clear),
-]
